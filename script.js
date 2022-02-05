87c9c21115aedc94b8c608c2766b821cee3d6de3
Luis Eduardo Mayida González <51863317+lalomayida@users.noreply.github.com>
2022-02-04 19:23:22 -0600

    Adding navigation to element

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,9 +32,6 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', e => {
   //Old way
   // const s1coords = section1.getBoundingClientRect();
@@ -44,4 +43,14 @@ btnScrollTo.addEventListener('click', e => {
 
   //New way
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//Page navigation
+
+document.querySelectorAll('.nav__link').forEach(navLink => {
+  navLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
 });
